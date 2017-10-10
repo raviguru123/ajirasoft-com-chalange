23f656c63d71b9b1bea765f52d41aa6af33bb776
raviguru007 <raviguru@goparties.com>
2017-10-10 20:13:11 +0530

    http://practice.geeksforgeeks.org/problems/sort-an-array-of-0s-1s-and-2s/0/?ref=self

---
+++ Sort_ an_ array_of _0s_1s_and 2s.py
@@ -0,0 +1,43 @@
+#code
+
+def sort_zeros_ones_twos(list):
+    zero_count=0;
+    one_count=0;
+    two_count=0;
+    output=[];
+    for i in list:
+        if(i==0):
+            zero_count+=1;
+        if(i==1):
+            one_count+=1;
+        if(i==2):
+            two_count+=1;
+    for i in range(zero_count+one_count+two_count):
+        if(zero_count>0):
+            zero_count-=1;
+            output.append(0);
+        elif(one_count>0):
+            one_count-=1;
+            output.append(1);
+        else:
+            two_count-=1;
+            output.append(2);
+    return output;
+    
+def main():
+    dict={};
+    test=int(input());
+    for i in range(test):
+        list1=[];
+        length=input();
+        list1=list(map(int,input().split()));
+        dict[i]=sort_zeros_ones_twos(list1);
+    
+    for i in dict.keys():
+        for j in dict[i]:
+            print(j,end=' ');
+        print();
+
+if(__name__=='__main__'):
+    main();
+            
