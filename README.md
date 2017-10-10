e06551fdcfa24a12d3fd3cd38c7baeb646a0aa19
raviguru007 <raviguru@goparties.com>
2017-10-10 14:01:54 +0530

    Add unit test functionality and description in README.md file

---
+++ README.md
@@ -0,0 +1,5 @@
+This program is writen in python programming language and this is my first day of python programming language.
+
+I am going to use your input format according to your guidline, but your input format and test cases seems different, so i am finally use, your input format after correction of test cases.
+
+please run assignment.py file in python 3 environment.
--- assignment1.py
+++ assignment1.py
@@ -1,11 +1,12 @@
 # function is use for find successfull attack
 def find_successfull_attack(days):
     dict={};#this dictionary is use to track height of wall.
+    #ititially all side of wall length is zero.
     dict['N']=0;
     dict['S']=0;
     dict['E']=0;
     dict['W']=0;
-    #initially count of attack should be .
+    #initially count of attack should be zero.
     attackcount=0; 
 
     for day in days:#iterate days 
@@ -18,13 +19,21 @@ def find_successfull_attack(days):
             if(dict[direction]<force):
                 attackcount+=1;
 
-        for direction,force in dict2.items():
+        for direction,force in dict2.items():# made wall after single day attack
             if(dict[direction]<force):
                 dict[direction]=force;
             
                
        
     return attackcount;
+
+# this function is use for unit test
+def test(got, expected):
+    if got == expected:
+        prefix = ' OK '
+    else:
+        prefix = ' WRONG OUTPUT'
+    print ('%s   got: %s expected: %s' % (prefix, repr(got), repr(expected)));
 
 
 
@@ -35,9 +44,11 @@ def main():
     for i in range(3):
         day=input();
         days.append(day);
-  
-    print('<--------------Output---------------->')
-    print(find_successfull_attack(days));
+    print();
+    Output=int(input('Enter Expected Output='));
+    print();
+    print('<--------------Unit Test Output---------------->')
+    test(find_successfull_attack(days),Output);
     
 
 
