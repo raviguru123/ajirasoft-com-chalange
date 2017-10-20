793cbf4adf881563c913cfd4b8330e5f7dba86b1
raviguru007 <raviguru@goparties.com>
2017-10-20 22:08:46 +0530

    tree implement with three traversal method

---
+++ Tree/implement_tree.py
@@ -0,0 +1,76 @@
+class node:
+    def __init__(self,data):
+        self.data=data;
+        self.left=None;
+        self.right=None;
+
+
+    def insert(self,data):
+        if(self.data>data):
+            if(self.left):
+                self.left.insert(data);
+            else:
+                self.left=node(data);
+        else:
+            if(self.right):
+                self.right.insert(data);
+            else:
+                self.right=node(data);
+
+    def inoder(self):
+        if(self.left):
+            self.left.inoder();
+        print(self.data,end=' ');
+        if(self.right):
+            self.right.inoder();       
+            
+    def preorder(self):
+        print(self.data,end=' ');
+        if(self.left):
+            self.left.preorder();
+        if(self.right):
+            self.right.preorder();
+    def postorder(self):
+        if(self.left):
+            self.left.postorder();
+        if(self.right):
+            self.right.postorder();
+        print(self.data,end=' ');
+
+class Tree:
+    def __init__(self):
+        self.root=None;
+    def insert(self,data):
+        if(self.root):
+            self.root.insert(data);
+        else:
+            self.root=node(data);
+
+    def inorder(self):
+        print("<----------inorder traversal start------------>");
+        self.root.inoder();
+        print();
+        print('-----------------------------------------------');
+    def preorder(self):
+        print('<----------preorder traversal start------------>');
+        self.root.preorder();
+        print();
+        print('-----------------------------------------------');
+    def postorder(self):
+        print('<----------postorder traversal start------------>');
+        self.root.postorder();
+        print();
+        print('-----------------------------------------------');
+
+bst=Tree();
+bst.insert(10);
+bst.insert(5);
+bst.insert(15);
+bst.insert(4);
+bst.insert(6);
+bst.insert(11);
+bst.insert(16);
+bst.insert(9);
+bst.inorder();
+bst.preorder();
+bst.postorder();
